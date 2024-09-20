46ff9661b092633eaa2ec38cbcae349dcb9142f3
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-20 10:11:40 +0530

    Create 1.c

---
+++ SET 1/c output/1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int n;
+for(n = 7; n!=0; n--)
+	printf("n = %d", n--);
+getchar();
+return 0;
+}
+
+
+Output:Above program goes in infinite loop because n is never zero when loop condition (n != 0) is checked.
