a69fdd7850a0f1510813464aa10b448bcb921cee
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-20 10:12:25 +0530

    Create 3.c

---
+++ SET 1/c output/3.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+# define scanf  "%s Geeks For Geeks "
+main()
+{
+   printf(scanf, scanf);
+   getchar();
+   return 0;
+}
+
+
+Output: %s Geeks For Geeks Geeks For Geeks 
+Explanation: After pre-processing phase of compilation, printf statement will become. 
